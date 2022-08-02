b4b0a50b05e5e5071126160712eaab56f77babce
Luis Fernando Sanchez Barrera <lic.luisfsanchezbarrera@gmail.com>
2022-08-02 11:13:47 -0500

    Agregando Aplicacion

--- Leccion5SumasDePrefijo/PassingCars/PassingCarsAplicacion/Program.cs
+++ Leccion5SumasDePrefijo/PassingCars/PassingCarsAplicacion/Program.cs
@@ -1,2 +1,6 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿using PassingCarsBiblioteca;
+
+Console.WriteLine("Carros que se Pasan");
+Algoritmo algoritmo = new Algoritmo();
+int[] A = { 0, 1, 0, 1, 1 };
+algoritmo.Solucion(A);
