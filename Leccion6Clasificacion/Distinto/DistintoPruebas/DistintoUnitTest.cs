72ea693a397c4be8e101ab71a0f272a936058455
Luis Fernando Sanchez Barrera <lic.luisfsanchezbarrera@gmail.com>
2022-08-02 18:16:24 -0500

    Creando Pruebas unitarias

---
+++ Leccion6Clasificacion/Distinto/DistintoPruebas/DistintoUnitTest.cs
@@ -0,0 +1,20 @@
+namespace DistintoPruebas
+{
+    [TestClass]
+    public class DistintoUnitTest
+    {
+        [TestMethod]
+        public void pruebaA211231obten3()
+        {
+            //Arrange = Arreglar o adecuar la PruebaUnitaria
+            Algoritmo algAutosConsecutivos = new Algoritmo();
+            int[] A_evaluado = {2,1,1,2,3,1};
+            int obtenido;
+            int esperado = 3;
+            //Act = Actuar o realizar la prueba
+            obtenido = algAutosConsecutivos.Solucion(A_evaluado);
+            //Assert = Afirmar o verificar que la prueba cumple la condicion
+            Assert.AreEqual(obtenido, esperado);
+        }
+    }
+}
