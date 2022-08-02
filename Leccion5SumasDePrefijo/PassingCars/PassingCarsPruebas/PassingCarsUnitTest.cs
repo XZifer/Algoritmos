8f02ab96d5ee76c220bb01c9db887516dca2ede2
Luis Fernando Sanchez Barrera <lic.luisfsanchezbarrera@gmail.com>
2022-08-02 10:11:27 -0500

    Agregando Proyecto de Pruebas Unitarias

---
+++ Leccion5SumasDePrefijo/PassingCars/PassingCarsPruebas/PassingCarsUnitTest.cs
@@ -0,0 +1,11 @@
+namespace PassingCarsPruebas
+{
+    [TestClass]
+    public class PassingCarsUnitTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
