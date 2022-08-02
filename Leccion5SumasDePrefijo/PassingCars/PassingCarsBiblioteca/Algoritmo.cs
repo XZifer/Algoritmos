15e581daf92747a19bc9eb1d97d542f49b1d025e
Luis Fernando Sanchez Barrera <lic.luisfsanchezbarrera@gmail.com>
2022-08-02 17:57:45 -0500

    Optimizacion Algoritmo PassingCars

--- Leccion5SumasDePrefijo/PassingCars/PassingCarsBiblioteca/Algoritmo.cs
+++ Leccion5SumasDePrefijo/PassingCars/PassingCarsBiblioteca/Algoritmo.cs
@@ -1,25 +1,117 @@
-﻿namespace PassingCarsBiblioteca
+﻿using System.Linq;
+
+namespace PassingCarsBiblioteca
 {
     public class Algoritmo
     {
         public int Solucion(int[] A)
         {
-            int contador = 0;
-            for (int p = 0; p < A.Length; p++)
+            #region Opcion1
+            //int contador = 0;
+            //for (int p = 0; p < A.Length - 1; p++)
+            //{
+            //    for (int q = p +1; q < A.Length; q++)
+            //    {
+            //        if (A[p] == 0 && A[q] == 1)
+            //        {
+            //            contador++;
+            //            if (contador > 1000000000)
+            //            {
+            //                return -1;
+            //            }
+            //        }
+            //    }
+            //}
+            //return contador;
+            #endregion
+            #region Opcion2
+            //int contador = 0;
+            //for (int p = 0; p < A.Length -1; p++)
+            //{
+            //    if (A[p] == 0)
+            //    {
+            //        for (int q = p + 1; q < A.Length; q++)
+            //        {
+            //            if (A[q] == 1)
+            //            {
+            //                contador++;
+            //                if (contador > 1000000000)
+            //                {
+            //                    return -1;
+            //                }
+            //            }
+            //        }
+            //    }
+            //}
+            //return contador;
+            #endregion
+            #region Opcion3
+            //int contador = 0;
+            //for (int p = 0; p < A.Length -1; p++)
+            //{
+            //    if (A[p] == 0)
+            //    {
+            //        int q = p + 1;
+            //        int[] array = new int[A.Length - q];
+            //        Array.Copy(A,q,array,0,A.Length-q);
+            //        contador += array.Sum();
+            //        if (contador > 1000000000)
+            //        {
+            //            return -1;
+            //        }
+            //    }
+            //}
+            //return contador;
+            #endregion
+            #region Opcion4
+            //int contador = 0;
+            //for (int i = 0; i < A.Length; i++)
+            //{
+            //    if (A[i] == 0)
+            //    {
+            //        for (int j = i; j < A.Length; j++)
+            //        {
+            //            if (A[j] == 1)
+            //            {
+            //                contador += 1;
+            //            }
+            //        }
+            //    }
+            //}
+            //if (contador >= 1000000000)
+            //{
+            //    return -1;
+            //}
+            //else
+            //{
+            //    return contador;
+            //}
+            #endregion
+            int l = A.Length;
+            int countZero = 0;
+            int countPairs = 0;
+            int exceed = 1000000000;
+
+            for (int i = 0; i < l; i++)
             {
-                for (int q = 1; q < A.Length; q++)
+                if (A[i] == 1)
                 {
-                    if (p < q && A[p] == 0 && A[q] == 1)
-                    {
-                        contador++;
-                    }
+                    countPairs += countZero;
+                }
+                else
+                {
+                    countZero++;
                 }
             }
-            if (contador > 1000000)
+
+            if (countPairs > exceed || countPairs < 0)
             {
-                contador = -1;
+                return -1;
             }
-            return contador;
+            else
+            {
+                return countPairs;
+            }
         }
     }
 }
