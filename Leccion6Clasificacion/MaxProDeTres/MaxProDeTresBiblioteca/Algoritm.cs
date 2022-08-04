969f9362ebe619d2d8cf01072333293ae22aa73d
Luis Fernando Sanchez Barrera <lic.luisfsanchezbarrera@gmail.com>
2022-08-04 13:41:27 -0500

    Agregando Algoritmo

--- Leccion6Clasificacion/MaxProDeTres/MaxProDeTresBiblioteca/Algoritm.cs
+++ Leccion6Clasificacion/MaxProDeTres/MaxProDeTresBiblioteca/Algoritm.cs
@@ -2,9 +2,26 @@
 {
     public class Algoritm
     {
-        public int Solucion(int[] a_evaluada)
+        public int Solucion(int[] A)
         {
-            throw new NotImplementedException();
+            int[] B = A;
+            int masGrande;
+            int max = 0;
+            Array.Sort(B);
+            masGrande = A.Max();
+            int ladoNegativo = B[0]*B[1];
+            int ladoPositivo = B[B.Length - 2] * B[B.Length - 3];
+            if (ladoNegativo > ladoPositivo)
+            {
+                max = masGrande * ladoNegativo;
+                return max;
+            }
+            else
+            {
+                max = masGrande * ladoPositivo;
+                return max;
+            }
+            
         }
     }
 }
