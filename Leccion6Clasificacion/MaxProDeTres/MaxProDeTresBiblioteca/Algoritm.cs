53ba5c974877673acf5000f1d605ccb70b857f44
Luis Fernando Sanchez Barrera <lic.luisfsanchezbarrera@gmail.com>
2022-08-04 14:09:14 -0500

    Correccion del Algoritmo

--- Leccion6Clasificacion/MaxProDeTres/MaxProDeTresBiblioteca/Algoritm.cs
+++ Leccion6Clasificacion/MaxProDeTres/MaxProDeTresBiblioteca/Algoritm.cs
@@ -4,23 +4,28 @@
     {
         public int Solucion(int[] A)
         {
-            int[] B = A;
             int masGrande;
             int max = 0;
-            Array.Sort(B);
+            Array.Sort(A);
             masGrande = A.Max();
-            int ladoNegativo = B[0]*B[1];
-            int ladoPositivo = B[B.Length - 2] * B[B.Length - 3];
-            if (ladoNegativo > ladoPositivo)
+            int extremoIzq = A[0]*A[1];
+            int extremoDer = A[A.Length - 2] * A[A.Length - 3];
+            if (masGrande > 0)
             {
-                max = masGrande * ladoNegativo;
-                return max;
+                if (extremoIzq > extremoDer)
+                {
+                    max = masGrande * extremoIzq;
+                }
+                else
+                {
+                    max = masGrande * extremoDer;
+                }
             }
             else
             {
-                max = masGrande * ladoPositivo;
-                return max;
+                max = masGrande * extremoDer;
             }
+            return max;
             
         }
     }
