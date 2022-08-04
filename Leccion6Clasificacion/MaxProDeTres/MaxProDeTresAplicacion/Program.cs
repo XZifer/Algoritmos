c4d815d9ea1931b5b1bfd9cdb9b94b4390a9772e
Luis Fernando Sanchez Barrera <lic.luisfsanchezbarrera@gmail.com>
2022-08-04 11:45:48 -0500

    Agragando Aplicacion

--- Leccion6Clasificacion/MaxProDeTres/MaxProDeTresAplicacion/Program.cs
+++ Leccion6Clasificacion/MaxProDeTres/MaxProDeTresAplicacion/Program.cs
@@ -1,1 +1,16 @@
-﻿Console.WriteLine("Maximo Producto De Tres Por Indices");
+﻿using MaxProDeTresBiblioteca;
+
+Console.WriteLine("Maximo Producto De Tres, Por Indices De Un Arreglo");
+Algoritm algoritm = new Algoritm();
+int[] A = { -3, 1, 2, -2, 5, 6 };
+int max;
+Console.Write("{");
+for (int i = 0; i < A.Length; i++)
+{
+    Console.Write(" {0}", A[i]);
+}
+Console.Write(" }");
+max = algoritm.Solucion(A);
+Console.WriteLine("El Maximo es: {0}",max);
+
+
