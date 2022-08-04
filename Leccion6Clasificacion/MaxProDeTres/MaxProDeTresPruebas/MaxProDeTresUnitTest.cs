f9f00421344e3d20b39fcdca3b0ac849584e717d
Luis Fernando Sanchez Barrera <lic.luisfsanchezbarrera@gmail.com>
2022-08-04 10:44:12 -0500

    Creando Proyecto MaxProDeTresPrueba

---
+++ Leccion6Clasificacion/MaxProDeTres/MaxProDeTresPruebas/MaxProDeTresUnitTest.cs
@@ -0,0 +1,11 @@
+namespace MaxProDeTresPruebas
+{
+    [TestClass]
+    public class MaxProDeTresUnitTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
