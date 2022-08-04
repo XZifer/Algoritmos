2f55c73f6ad1a3764652d0823ca445e476e0be90
Luis Fernando Sanchez Barrera <lic.luisfsanchezbarrera@gmail.com>
2022-08-04 11:12:48 -0500

    Agregando Pruebas Unitarias

--- Leccion6Clasificacion/MaxProDeTres/MaxProDeTresBiblioteca/Algoritm.cs
+++ Leccion6Clasificacion/MaxProDeTres/MaxProDeTresBiblioteca/Algoritm.cs
@@ -2,6 +2,9 @@
 {
     public class Algoritm
     {
-
+        public int Solucion(int[] a_evaluada)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- Leccion6Clasificacion/MaxProDeTres/MaxProDeTresPruebas/MaxProDeTresUnitTest.cs
+++ Leccion6Clasificacion/MaxProDeTres/MaxProDeTresPruebas/MaxProDeTresUnitTest.cs
@@ -1,11 +1,22 @@
+using MaxProDeTresBiblioteca;
+
 namespace MaxProDeTresPruebas
 {
     [TestClass]
     public class MaxProDeTresUnitTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void PruebaA_312_256Obten60()
         {
+            //Arrange
+            Algoritm algoritm = new Algoritm();
+            int[] A_evaluada = { -3, 1, 2, -2, 5, 6 };
+            int obtenido;
+            int esperado = 60;
+            //Act
+            obtenido = algoritm.Solucion(A_evaluada);
+            //Assert
+            Assert.AreEqual(obtenido, esperado);
         }
     }
 }
