e89b75a6c2dc4d5a2339725c75fb7dc3dbbdc231
Luis Fernando Sanchez Barrera <lic.luisfsanchezbarrera@gmail.com>
2022-10-04 16:23:32 -0500

    Correcion de Algorit en el tope de las iteraciones

--- Leccion6Clasificacion/Triangle/TriangleLibrary/Algorit.cs
+++ Leccion6Clasificacion/Triangle/TriangleLibrary/Algorit.cs
@@ -5,7 +5,20 @@
         public int Triangulo(int[] A)
         {
             int resultado = 0;
-
+            for (int p = 0; p < A.Length - 2; p++)
+            {
+                for (int q = 1; q < A.Length - 1; q++)
+                {
+                    for (int r = 2; r < A.Length; r++)
+                    {
+                        if (A[p] + A[q] > A[r] && A[q] + A[r] > A[p] && A[r] + A[p] > A[q])
+                        {
+                            resultado = 1;
+                            return resultado;
+                        }
+                    }
+                }
+            }
             return resultado;
         }
     }
