417b06c0a2526beae470c4f50e4de42bfba6d2d2
Luis Fernando Sanchez Barrera <lic.luisfsanchezbarrera@gmail.com>
2022-10-04 11:05:03 -0500

    Agregando Pruebas unitarias TriangleTest

--- Leccion6Clasificacion/Triangle/TriangleTest/TriangleUnitTest.cs
+++ Leccion6Clasificacion/Triangle/TriangleTest/TriangleUnitTest.cs
@@ -1,11 +1,30 @@
+using TriangleLibrary;
+
 namespace TriangleTest
 {
     [TestClass]
     public class TriangleUnitTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Prueba10251820Regresa1()
         {
+            Algorit al = new();
+            int[] A_evaluar = { 10,2,5,1,8,20 };
+            int obtenido;
+            int esperado = 1;
+            obtenido = al.Triangulo(A_evaluar);
+            Assert.AreEqual(obtenido, esperado);    
+        }
+
+        [TestMethod]
+        public void Prueba105051Regresa0()
+        {
+            Algorit al = new();
+            int[] A_evaluar = { 10,50,5,1 };
+            int obtenido;
+            int esperado = 0;
+            obtenido = al.Triangulo(A_evaluar);
+            Assert.AreEqual(obtenido, esperado);
         }
     }
 }
