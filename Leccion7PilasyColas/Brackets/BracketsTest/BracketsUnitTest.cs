130c3773e2fc5fa18586fe311f5d66e7852aaee6
Luis Fernando Sanchez Barrera <lic.luisfsanchezbarrera@gmail.com>
2022-10-06 20:51:47 -0500

    Agregando Pruebas Unitarias Brackets

--- Leccion7PilasyColas/Brackets/BracketsTest/BracketsUnitTest.cs
+++ Leccion7PilasyColas/Brackets/BracketsTest/BracketsUnitTest.cs
@@ -1,11 +1,29 @@
+using BracketsLibrary;
+
 namespace BracketsTest
 {
     [TestClass]
     public class BracketsUnitTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Prueba1Regresa1 ()
         {
+            Algoritm algoritm = new();
+            string S_evaluado = "{[()()]}";
+            int obtenido;
+            int esperado = 1;
+            obtenido = algoritm.Anidar(S_evaluado);
+            Assert.AreEqual(obtenido, esperado);    
+        }
+        [TestMethod]
+        public void Prueba2Regresa0()
+        {
+            Algoritm algoritm = new();
+            string S_evaluado = " ([)()] ";
+            int obtenido;
+            int esperado = 0;
+            obtenido = algoritm.Anidar(S_evaluado);
+            Assert.AreEqual(obtenido, esperado);
         }
     }
 }
