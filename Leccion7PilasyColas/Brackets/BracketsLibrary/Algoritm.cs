5110b33c93887ad1db0255d374003487629e1a1e
Luis Fernando Sanchez Barrera <lic.luisfsanchezbarrera@gmail.com>
2022-10-06 22:03:36 -0500

    Implementacion de Algoritm

--- Leccion7PilasyColas/Brackets/BracketsLibrary/Algoritm.cs
+++ Leccion7PilasyColas/Brackets/BracketsLibrary/Algoritm.cs
@@ -4,7 +4,21 @@
     {
         public int Anidar(string S)
         {
-            throw new NotImplementedException();
+            if (S == "" || S.Length%2 != 0)
+            {
+                return 0;
+            }
+            for (int i = 0,j = S.Length -1; i < j; i++, j--)
+            {
+                if ((S[i] == '(' && S[j] != ')') ||
+                    (S[i] == '{' && S[j] != '}') ||
+                    (S[i] == '[' && S[j] != ']'))
+                {
+                    return 0;
+                }
+                return 1;
+            }
+            return 0;
         }
     }
 }
