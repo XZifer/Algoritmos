2cc9f4c95b94f97f5fe0c571c8d9a5d37ac86d8a
Luis Fernando Sanchez Barrera <lic.luisfsanchezbarrera@gmail.com>
2022-10-10 17:36:19 -0500

    Agregando Prueba3Regresa0

--- Leccion7PilasyColas/Brackets/BracketsTest/BracketsUnitTest.cs
+++ Leccion7PilasyColas/Brackets/BracketsTest/BracketsUnitTest.cs
@@ -25,5 +25,15 @@ namespace BracketsTest
             obtenido = algoritm.Anidar(S_evaluado);
             Assert.AreEqual(obtenido, esperado);
         }
+        [TestMethod]
+        public void Prueba3Regresa0()
+        {
+            Algoritm algoritm = new();
+            string S_evaluado = ")(";
+            int obtenido;
+            int esperado = 0;
+            obtenido = algoritm.Anidar(S_evaluado);
+            Assert.AreEqual(obtenido, esperado);
+        }
     }
 }
