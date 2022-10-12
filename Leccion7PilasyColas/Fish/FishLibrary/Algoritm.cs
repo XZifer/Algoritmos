3616975bdc141796c253a7b2230e78049b253b68
Luis Fernando Sanchez Barrera <lic.luisfsanchezbarrera@gmail.com>
2022-10-11 22:19:04 -0500

    Optimizacion de Algoritmo de FishLibrary

--- Leccion7PilasyColas/Fish/FishLibrary/Algoritm.cs
+++ Leccion7PilasyColas/Fish/FishLibrary/Algoritm.cs
@@ -2,9 +2,26 @@
 {
     public class Algoritm
     {
-        public int LiveFish(int[] a, int[] b)
+        public int LiveFish(int[] A, int[] B)
         {
-            throw new NotImplementedException();
+            int lives = 0;
+            for (int i = 0; i < A.Length; i++)
+            {
+                if (i == 0 && B[i] == 0 ||
+                    i == A.Length - 1 && B[i] == 1)
+                {
+                    lives++;
+                    A[i] = 0;
+                }
+                else 
+                {
+                    for (int j = i + 1; j < A.Length; j++)
+                    {
+
+                    }
+                }
+            }
+            return lives;
         }
     }
 }
