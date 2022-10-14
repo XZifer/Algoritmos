54d367c2f3d91c7ecfa94c3a537b7e2b96e88372
Luis Fernando Sanchez Barrera <lic.luisfsanchezbarrera@gmail.com>
2022-10-14 18:56:01 -0500

    Agregando Prueba Unitaria 4

--- Leccion7PilasyColas/Anidados/AnidadosTest/AnidadosUnitTest.cs
+++ Leccion7PilasyColas/Anidados/AnidadosTest/AnidadosUnitTest.cs
@@ -25,5 +25,25 @@ namespace AnidadosTest
             obtenido = algoritm.Anida(S_evaluado);
             Assert.AreEqual(obtenido, esperado);
         }
+        [TestMethod]
+        public void Prueba3Regresa0()
+        {
+            Algoritm algoritm = new();
+            string S_evaluado = ")(";
+            int obtenido;
+            int esperado = 0;
+            obtenido = algoritm.Anida(S_evaluado);
+            Assert.AreEqual(obtenido, esperado);
+        }
+        [TestMethod]
+        public void Prueba4Regresa1()
+        {
+            Algoritm algoritm = new();
+            string S_evaluado = "()()()(()(())()())()";
+            int obtenido;
+            int esperado = 1;
+            obtenido = algoritm.Anida(S_evaluado);
+            Assert.AreEqual(obtenido, esperado);
+        }
     }
 }
