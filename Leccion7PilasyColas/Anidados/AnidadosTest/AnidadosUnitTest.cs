c303c4e592e64abc7917c46180643b2d56ff4a82
Luis Fernando Sanchez Barrera <lic.luisfsanchezbarrera@gmail.com>
2022-10-14 15:18:12 -0500

    Agregando Pruebas Unitarias

--- Leccion7PilasyColas/Anidados/AnidadosTest/AnidadosUnitTest.cs
+++ Leccion7PilasyColas/Anidados/AnidadosTest/AnidadosUnitTest.cs
@@ -1,11 +1,29 @@
+using AnidadosLibrary;
+
 namespace AnidadosTest
 {
     [TestClass]
     public class AnidadosUnitTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Prueba1Regresa1()
         {
+            Algoritm algoritm = new();
+            string S_evaluado = "(()())";
+            int obtenido;
+            int esperado = 1;
+            obtenido = algoritm.Anida(S_evaluado);
+            Assert.AreEqual(obtenido, esperado);    
+        }
+        [TestMethod]
+        public void Prueba2Regresa0()
+        {
+            Algoritm algoritm = new();
+            string S_evaluado = "(()";
+            int obtenido;
+            int esperado = 0;
+            obtenido = algoritm.Anida(S_evaluado);
+            Assert.AreEqual(obtenido, esperado);
         }
     }
 }
