05d9a672630c5240355d636cb451d5275c1d0da0
Luis Fernando Sanchez Barrera <lic.luisfsanchezbarrera@gmail.com>
2022-10-14 17:18:29 -0500

    Implementando Algoritm en AnidadosLibrary

--- Leccion7PilasyColas/Anidados/AnidadosLibrary/Algoritm.cs
+++ Leccion7PilasyColas/Anidados/AnidadosLibrary/Algoritm.cs
@@ -2,6 +2,35 @@
 {
     public class Algoritm
     {
-
+        public int Anida(string S)
+        {
+            Stack<char> open = new Stack<char>();
+            Stack<char> close = new Stack<char>();
+            if (S == "")
+            {
+                return 1;
+            }
+            foreach (char c in S)
+            {
+                if (c == '(')
+                {
+                    open.Push(c);
+                }
+                else if (c == ')')
+                {
+                    close.Push(c);
+                }
+                while (open.Count > 0 && close.Count > 0)
+                {
+                    open.Pop();
+                    close.Pop();
+                }
+            }
+            if (open.Count + close.Count > 0)
+            {
+                return 1;
+            }
+            return 0;
+        }
     }
 }
